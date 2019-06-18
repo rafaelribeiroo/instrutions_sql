3aae952476710fa49a17409c6dd13fa573072c69
Rafael <pereiraribeirorafael@gmail.com>
2019-06-18 13:58:27 -0300

    Arquivos úteis com explicações
    Dicas e truques juntamente com um arquivo de questionário que fui pesquisando no stack e agregando

--- .sql/Tips and tricks.sql
+++ .sql/Tips and tricks.sql
@@ -4,7 +4,7 @@
 -- inúteis, para resolver esse "problema", o Postgres nos disponibiliza...
 
 -- Ver quanto espaço está em utilização no DB
-SELECT pg_size_pretty( pg_database_size('<name_db>') );
+SELECT pg_size_pretty( pg_database_size('<name_db>'));
 -- Ver quanto armazenamento estou usando na tabela
-SELECT pg_size_pretty( pg_total_relation_size('<table_name>') );
+SELECT pg_size_pretty( pg_total_relation_size('<table_name>'));
 VACUUM(FULL, ANALYZE, VERBOSE) <table_name>(column);
